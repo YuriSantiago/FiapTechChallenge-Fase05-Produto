2a2bd7792968e2ef7f4bc399450814e5a061edf1
YuriSantiago <yuri_santiago18@hotmail.com>
2024-12-23 15:46:30 -0300

    Implementação do Seed

--- FiapTechChallenge.IntegrationTests/CustomWebApplicationFactory.cs
+++ FiapTechChallenge.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,10 +1,9 @@
-﻿using FiapTechChallenge.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Hosting;
+﻿using FiapTechChallenge.Core.Entities;
+using FiapTechChallenge.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Data.Common;
 
 namespace FiapTechChallenge.IntegrationTests
@@ -24,20 +23,56 @@ namespace FiapTechChallenge.IntegrationTests
     //    }
     //}
 
+    //public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    //{
+    //    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    //    {
+    //        base.ConfigureWebHost(builder);
+
+    //        builder.ConfigureTestServices(services =>
+    //        {
+    //            // Remove o contexto de banco de dados existente
+    //            var dbContext = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+    //            if (dbContext is not null)
+    //                services.Remove(dbContext);
+
+    //            // Configura o SQLite em memória
+    //            services.AddDbContext<ApplicationDbContext>(options =>
+    //            {
+    //                options.UseSqlite("DataSource=:memory:");
+    //            });
+
+    //            // Certifique-se de inicializar o banco de dados antes de usá-lo
+    //            using var serviceProvider = services.BuildServiceProvider();
+    //            using var scope = serviceProvider.CreateScope();
+    //            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    //            db.Database.OpenConnection(); // Abre a conexão do SQLite em memória
+    //            db.Database.EnsureCreated(); // Garante que o esquema está criado
+    //        });
+
+    //        builder.UseEnvironment("Development");
+    //    }
+
+    //}
+
     public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             base.ConfigureWebHost(builder);
 
-            builder.ConfigureTestServices(services =>
+            builder.ConfigureTestServices(async services =>
             {
+                // Remove o DbContext existente
                 var dbContext = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
                 services.Remove(dbContext);
 
+                // Remove a conexão existente
                 var dbConnection = services.SingleOrDefault(x => x.ServiceType == typeof(DbConnection));
                 services.Remove(dbConnection);
 
+                // Configura a conexão SQLite em memória
                 services.AddSingleton<DbConnection>(container =>
                 {
                     var connection = new SqliteConnection("DataSource=:memory:");
@@ -48,12 +83,43 @@ namespace FiapTechChallenge.IntegrationTests
                 services.AddDbContext<ApplicationDbContext>((container, options) =>
                 {
                     var connection = container.GetRequiredService<DbConnection>();
-                    options.UseSqlServer(connection);
+                    options.UseSqlite(connection);
                 });
 
+                // Garante que o banco de dados seja seedado
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                await context.Database.EnsureCreatedAsync(); // Cria o esquema do banco de dados
+                await SeedDatabase(context); // Executa o Seed
             });
 
             builder.UseEnvironment("Development");
         }
+
+        private async Task SeedDatabase(ApplicationDbContext context)
+        {
+            // Truncate ou limpa os dados da tabela
+            await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE Regiao");
+            //await context.Database.ExecuteSqlRawAsync("DELETE FROM sqlite_sequence WHERE name='Regiao';");
+
+            // Insere os dados iniciais
+            context.Regioes.Add(new Regiao
+            {
+                DDD = 11,
+                Descricao = "São Paulo",
+                DataInclusao = DateTime.UtcNow
+            });
+
+            context.Regioes.Add(new Regiao
+            {
+                DDD = 21,
+                Descricao = "Rio de Janeiro",
+                DataInclusao = DateTime.UtcNow
+            });
+
+            await context.SaveChangesAsync();
+        }
     }
+
 }
