3020c5f910a1d86afea21cd10bd5c1a1e366c298
YuriSantiago <yuri_santiago18@hotmail.com>
2025-03-12 13:57:36 -0300

    Log de ID's

--- IntegrationTestsV2/CustomWebApplicationFactory.cs
+++ IntegrationTestsV2/CustomWebApplicationFactory.cs
@@ -59,12 +59,15 @@ namespace IntegrationTestsV2
 
             await context.SaveChangesAsync();
 
+            Console.WriteLine($"ID São Paulo: {regiaoSP.Entity.Id}");
+            Console.WriteLine($"ID Rio de Janeiro: {regiaoRJ.Entity.Id}");
+
             context.Contatos.Add(new Contato
             {
                 Nome = "Yuri",
                 Telefone = "999999999",
                 Email = "yuri@email.com",
-                RegiaoId = 1,
+                RegiaoId = regiaoSP.Entity.Id,
                 Regiao = regiaoSP.Entity
             });
 
@@ -73,11 +76,18 @@ namespace IntegrationTestsV2
                 Nome = "Yago",
                 Telefone = "999999999",
                 Email = "yago@email.com",
-                RegiaoId = 2,
+                RegiaoId = regiaoRJ.Entity.Id,
                 Regiao = regiaoRJ.Entity
             });
 
             await context.SaveChangesAsync();
+
+            // Logar os IDs dos contatos para verificação
+            var contatoYuri = context.Contatos.FirstOrDefault(c => c.Nome == "Yuri");
+            var contatoYago = context.Contatos.FirstOrDefault(c => c.Nome == "Yago");
+
+            Console.WriteLine($"ID Yuri: {contatoYuri?.Id}");
+            Console.WriteLine($"ID Yago: {contatoYago?.Id}");
         }
 
     }
